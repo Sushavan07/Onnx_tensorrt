c2b58f5b955b5ba8b93945e5d9ef3e38a1e5e812
Sushavan Mondal <31999424+Sushavan07@users.noreply.github.com>
2019-10-22 12:19:38 +0530

    Update onnxparser2.py

--- onnxparser2.py
+++ onnxparser2.py
@@ -1,3 +1,6 @@
+import tensorrt as trt
+import os
+TRT_LOGGER=trt.Logger(trt.Logger.WARNING)
 with builder = trt.Builder(TRT_LOGGER) as builder, builder.create_network() as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
     with open('resnet100.onnx', 'rb') as model:
         parser.parse(model.read())

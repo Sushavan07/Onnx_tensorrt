7489ddd3a2ff17cbad0e3cfc9144147dc1dcc687
Sushavan Mondal <31999424+Sushavan07@users.noreply.github.com>
2019-11-01 14:25:28 +0530

    Add files via upload

---
+++ onnx_parser.py
@@ -0,0 +1,7 @@
+import tensorrt as trt
+TRT_LOGGER=TRT.Logger(trt.Logger.WARNING)
+with builder=trt.Builder(TRT_LOGGER) as builder,builder.create_network() as network, trt.OnnxParser(network, TRT_LOGGER) as parser:
+    with open('resnet100.onnx','rb') as model:
+        parser.parse(model.read())
+
+        
